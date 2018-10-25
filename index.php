8743ba563e7ceebda813a37ecf3a98b821e49431
Matt <matthew.tapley@hotmail.co.uk>
2018-10-25 19:23:56 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,139 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DIGITAL ME</title>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" defer></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <header class="navContainer">
+        <nav class="mobNav">
+            <div></div>
+        </nav>
+        <nav class="lrgNav">
+            <ul>
+                <li>
+                    <a href="index.php">
+                        <div class="logoBtn headerContent">
+                            <img class="logo" src="images/mptLogo.jpg" />
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php">
+                        <div class="homeBtn headerContent">
+                            <p>HOME</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="cvBtn headerContent">
+                            <p>RÉSUMÉ</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="aboutMeBtn headerContent">
+                            <p>ABOUT ME</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="portfolioBtn headerContent">
+                            <p>PORTFOLIO</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="contactBtn headerContent">
+                            <p>GET IN TOUCH</p>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="hero">
+        <div class="languageIconsRowOne">
+            <img class="phpIcon" src="images/php7Logo.png" />
+            <img class="jsIcon" src="images/jsLogo.png" />
+            <img class="bootstrapIcon" src="images/bootstrapLogo.png" />
+            <img class="handlebarsIcon" src="images/handlebarsLogo.png" />
+
+        </div>
+        <div class="languageIconsRowTwo">
+            <img class="jqueryIcon" src="images/jqueryLogo.png" />
+            <img class="scrumIcon" src="images/scrumLogo.png" />
+            <img class="mysqlIcon" src="images/mysqlLogo.png" />
+            <img class="" src="" />
+        </div>
+    </div>
+    <div class=" container-fluid profile">
+        <div class="cvBoxTopHalf">
+            <img class="cv" src="images/cv.png" />
+        </div>
+        <div class="cvBoxBottomHalf"></div>
+        <div class="heroTextOne">
+            <img class="heroLogo" src="images/mptLogoLg.png" />
+        </div>
+        <div class="heroTextTwo">
+            <p>FULL-STACK DEVELOPMENT</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 profileText">
+                <p>HELLO, I'M
+                    <span class="highlightText">MATTHEW TAPLEY</span>
+                    , A 32-YEAR-OLD,
+                    <span class="highlightText">FULL-STACK DEVELOPER</span>
+                    STUDYING AT
+                    <span class="highlightText">MAYDEN ACADEMY</span>
+                    , BATH.
+                </p>
+            </div>
+        </div>
+        <div class="preCodingHistoryBg">
+            <div class="preCodingHistory">
+                <p class="storySoFar">MY STORY SO FAR</p>
+                <div class="hospitality">
+                    <p class="highlightText">APART FROM A BRIEF FORAY INTO CODING IN MY LATE<p>
+                    <p class="highlightText">TEENS, MY CAREER UP UNTIL NOW HAS BEEN<p>
+                    <p class="highlightText"> EXCLUSIVELY IN THE HOSPITALITY INDUSTRY, RUNNING<p>
+                    <p class="highlightText"> PUBS, BARS AND RESTAURANTS, LOOKING AFTER<p>
+                    <p class="highlightText"> PEOPLE AND CREATING MEMORABLE EXPERIENCES.</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="leftCol">
+                    <p class="maydenTitle">MY TIME AT MAYDEN ACADEMY</p>
+                    <img class="mayden" src="images/maydenAcademyLogo.jpg" />
+                </div>
+                <div class="rightCol">
+                    <div class="maydenText">
+                        <p class="highlightTextTwo">4 MONTHS INTENSIVE LEARNING</p>
+                        <p>TOPICS COVERED:</p>
+                        <p>HTML & CSS</p>
+                        <p>SCRUM & SOFTWARE DEVELOPER ESSENTIALS</p>
+                        <p>PHP BASICS</p>
+                        <p>DATABASES & SQL</p>
+                        <p>JAVASCRIPT BASICS</p>
+                        <p>ADVANCED FRONT END</p>
+                        <p>css preprocessors</p>
+                        <p>OOP WITH PHP</p>
+                        <p>MVC & PHP FRAMEWORKS</p>
+                        <p>JS W NODE OR ZEND</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
