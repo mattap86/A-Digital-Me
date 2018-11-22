f5c8b63d2142bdce8c99b72149393104246db5c6
Matt <matthew.tapley@hotmail.co.uk>
2018-11-22 21:01:49 +0000

    removed unused procedural code

--- app/php/editStory.php
+++ app/php/editStory.php
@@ -30,31 +30,8 @@ $db = dbconn();
 //    updateAboutMe($validatedStory_p1, $validatedStory_p2, $validatedStory_p3, $validatedStory_p4, $validatedStory_p5, $db);
 //}
 
-//set of if statements to sanitise new project inputs from admin.php
-//if (isset($_POST['newProjectName'])) {
-//    $validatedProjectName = filter_var($_POST['newProjectName'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newImageUrl'])) {
-//    $validatedImageUrl = filter_var($_POST['newImageUrl'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newProjectUrl'])) {
-//    $validatedProjectUrl = filter_var($_POST['newProjectUrl'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newProjectSummary'])) {
-//    $validatedProjectSummary = filter_var($_POST['newProjectSummary'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($validatedProjectName, $validatedImageUrl, $validatedProjectUrl, $validatedProjectSummary, $db)) {
-//    uploadProject($validatedProjectName, $validatedImageUrl, $validatedProjectUrl, $validatedProjectSummary, $db);
-//}
 
 $aboutMeResult = getDbAboutMe($db);
 
 $aboutMe = selectAboutMeFromResults($aboutMeResult);
 $preCodingHistory = selectPreCodingHistoryFromResults($aboutMeResult);
-//$story_p3 = selectStory_p3FromResults($aboutMeResult);
-//$story_p4 = selectStory_p4FromResults($aboutMeResult);
-//$story_p5 = selectStory_p5FromResults($aboutMeResult);
