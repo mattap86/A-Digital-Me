3062ab6abcdf43d9f6b3e4661066f72def960e61
Matt <matthew.tapley@hotmail.co.uk>
2018-11-24 22:14:54 +0000

    commented funcs and event listeners

--- app/js/hamburger.js
+++ app/js/hamburger.js
@@ -1,18 +1,19 @@
-//Brings the nav bar in from the right side of the viewport on clicking the hamburger icon.
+//Sets the width of the element with id="sidebar" to 200px.
 function openNav() {
     document.getElementById('sideBar').style.width = '200px';
 }
 
+//Adds event listener on click of hamburger icon to open sidebar.
 document.getElementById('hamburger').addEventListener('click', function() {
     openNav()
 })
 
-//Closes the nav bar on clicking x.
+//Sets the width of the element with id="sidebar" to 0.
 function closeNav() {
     document.getElementById('sideBar').style.width = '0';
 }
 
+//Adds event listener on click of X in sidebar to close sidebar.
 document.getElementById('closeSideNav').addEventListener('click', function() {
     closeNav()
 })
-
