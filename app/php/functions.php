34e6336de7e41479f688e6e374b40d8d67bd79e8
Matt <matthew.tapley@hotmail.co.uk>
2018-11-24 23:37:44 +0000

    added login functions and edited docblocks

--- app/php/functions.php
+++ app/php/functions.php
@@ -110,3 +110,49 @@ function displayProjects(array $projectResults) : string {
     }
     return $result;
 }
+
+/**
+ * Retrieves the first row of user credentials from the db.
+ *
+ * @param PDO $db is the db connection.
+ * @param string $userName is the input user of the login page.
+ *
+ * @return array contains the data retrieved by the query.
+ */
+function getCredentials(PDO $db, string $userName) : array {
+    $userQuery = $db->prepare("SELECT `username`, `password` FROM `users` WHERE `username` = :username;");
+    $userQuery->bindParam(':username', $userName);
+    $userQuery->execute();
+    return $userQuery->fetch();
+}
+
+/**
+ * Sanitises a string by filtering out any hidden/invalid characters.
+ *
+ * @param string $string is any string requiring sanitisation.
+ *
+ * @return string $sanitised is the string after sanitisation.
+ */
+function sanitise(string $string) : string {
+    $sanitised = filter_var($string, FILTER_SANITIZE_STRING);
+    return $sanitised;
+}
+
+/**
+ * Sets loggedIn to 1 in session if entered username and password match the expected data in the db.
+ *
+ * @param string $sanitisedUser is the entered username after sanitisation.
+ * @param string $username is the expected username pulled from the db.
+ * @param bool $verifiedPassword is the entered password, hashed, and checked against the db.
+ *
+ * @return bool
+ */
+function validateUser(string $sanitisedUser, string $username, bool $verifiedPassword) : bool {
+    if ($sanitisedUser === $username && $verifiedPassword === true) {
+        $_SESSION['loggedIn'] = 1;
+        return true;
+    } else {
+        $_SESSION['loggedIn'] = 0;
+        return false;
+    }
+}
