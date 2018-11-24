701be0ec5c7b047eab2256e01715b52a1227e151
Matt <matthew.tapley@hotmail.co.uk>
2018-11-24 23:39:17 +0000

    changed  and  to equal a no coalesce operator

--- app/php/login.php
+++ app/php/login.php
@@ -2,68 +2,27 @@
 
 session_start();
 
+require_once 'functions.php';
 require_once 'db.php';
 
-function getCredentials(PDO $db) : array {
-    $userQuery = $db->prepare("SELECT `username`, `password` FROM `users` WHERE `id` = 1;");
-    $userQuery->execute();
-    $userCredentials = $userQuery->fetch();
-    return $userCredentials;
-}
+$_SESSION['loggedIn'] = 0;
 
-function getUser(array $userCredentials) : string {
-    if (array_key_exists('username', $userCredentials)) {
-        return $userCredentials['username'];
-    }
-}
-
-function getPassword(array $userCredentials) : string {
-    if (array_key_exists('password', $userCredentials)) {
-        return $userCredentials['password'];
-    }
-}
-
-function sanitise(string $string) : string {
-    $sanitised = filter_var($string, FILTER_SANITIZE_STRING);
-    return $sanitised;
-}
-
-function verifyPassword($sanitisedPassword, $password) {
-    var_dump($sanitisedPassword);
-    if (password_verify($sanitisedPassword, $password)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validateUser(string $sanitisedUser, string $username, $verifiedPassword)
-{
-    if ($sanitisedUser == $username && $verifiedPassword == true) {
-        $_SESSION['loggedIn'] = 1;
-
-    } else {
-        $_SESSION['loggedIn'] = 0;
-    }
-}
-
-if(isset($_POST['username']) && isset($_POST['password'])){
+if(isset($_POST['username']) && isset($_POST['password'])) {
     $userName = $_POST['username'];
     $userPass = $_POST['password'];
 
     $db2 = dbConn2();
-    $userCredentials = getCredentials($db2);
-    $username = getUser($userCredentials);
-    $password = getPassword($userCredentials);
+    $userCredentials = getCredentials($db2, $userName);
+    $username = $userCredentials['username'] ?? false;
+    $password = $userCredentials['password'] ?? false;
 
     $sanitisedUsername = sanitise($userName);
     $sanitisedPassword = sanitise($userPass);
-    $verifiedPassword = verifyPassword($sanitisedPassword, $userCredentials['password']);
-    var_dump($verifiedPassword);
+    $verifiedPassword = password_verify($sanitisedPassword, $password);
     validateUser($sanitisedUsername, $username, $verifiedPassword);
 }
 
-if ($_SESSION['loggedIn'] == 1) {
+if ($_SESSION['loggedIn'] === 1) {
     header('Location: admin.php');
 }
 
@@ -73,19 +32,18 @@ if ($_SESSION['loggedIn'] == 1) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Log In | Matt Tapley | A Digital Me</title>
-
+    <title>CMS Log In | Matt Tapley | A Digital Me</title>
 </head>
 <body>
     <div class="container">
         <form class="loginBox" action="" method="post">
             <div class="userEntry">
                 <label>Username: </label>
-                <input type="text" name="username">
+                <input type="text" name="username" />
             </div>
             <div class="password">
                 <label>Password: </label>
-                <input type="password" name="password">
+                <input type="password" name="password" />
             </div>
             <div class="submit">
                 <input type="submit" value="Log In" />
