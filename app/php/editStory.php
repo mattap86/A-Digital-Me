bb7114c794c69fd70540b8370b19948ca151ef76
Matt <matthew.tapley@hotmail.co.uk>
2018-11-24 21:54:45 +0000

    updated if statements to sanitise  from admin page

--- app/php/editStory.php
+++ app/php/editStory.php
@@ -6,32 +6,20 @@ require_once 'db.php';
 $db = dbconn();
 
 //set of if statements to sanitise about me inputs from admin.php
-//if (isset($_POST['newStory_p1'])) {
-//    $validatedStory_p1 = filter_var($_POST['newStory_p1'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newStory_p2'])) {
-//    $validatedStory_p2 = filter_var($_POST['newStory_p2'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newStory_p3'])) {
-//    $validatedStory_p3 = filter_var($_POST['newStory_p3'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newStory_p4'])) {
-//    $validatedStory_p4 = filter_var($_POST['newStory_p4'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($_POST['newStory_p5'])) {
-//    $validatedStory_p5 = filter_var($_POST['newStory_p5'], FILTER_SANITIZE_STRING);
-//}
-//
-//if (isset($validatedStory_p1, $validatedStory_p2, $validatedStory_p3, $validatedStory_p4, $validatedStory_p5, $db)) {
-//    updateAboutMe($validatedStory_p1, $validatedStory_p2, $validatedStory_p3, $validatedStory_p4, $validatedStory_p5, $db);
-//}
+if (isset($_POST['newAboutMe'])) {
+    $validatedAboutMe = filter_var($_POST['newAboutMe'], FILTER_SANITIZE_STRING);
+}
+
+if (isset($_POST['newHistory'])) {
+    $validatedPreCodingHistory = filter_var($_POST['newHistory'], FILTER_SANITIZE_STRING);
+}
+
+if (isset($validatedAboutMe, $validatedPreCodingHistory, $db)) {
+    updateAboutMe($validatedAboutMe, $validatedPreCodingHistory, $db);
+}
 
 
-$aboutMeResult = getDbAboutMe($db);
+$aboutMeResult = getAboutMe($db);
 
 $aboutMe = selectAboutMeFromResults($aboutMeResult);
 $preCodingHistory = selectPreCodingHistoryFromResults($aboutMeResult);
